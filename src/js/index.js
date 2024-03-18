e38f4a00c5dab65a848da6e4854a46923a963b5e
Irskaraieva <mishytkina14@gmail.com>
2024-03-18 21:42:22 +0100

    add swiper breakpoints

--- src/js/index.js
+++ src/js/index.js
@@ -2,20 +2,35 @@ import mobileNav from './modules/mobile-nav.js';
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-const swiper = new Swiper('.swiper', {  
-   
-    slidesPerView: 2,
+const swiper = new Swiper('.swiper', {
+
+
     spaceBetween: 18,
 
+    breakpoints: {
+        900: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+        700: {
+            slidesPerView: 1.5,
+            slidesPerGroup: 1.5,
+        },
+        300: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        },
+    },
+
     navigation: {
-      nextEl: '.button-next',
-      prevEl: '.button-prev',
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
     },
-  
+
     scrollbar: {
-      el: '.swiper-scrollbar',
+        el: '.swiper-scrollbar',
     },
-  });
+});
 
 const nav = document.getElementById('nav');
 const navMobBtn = document.getElementById('nav-mob-btn');
