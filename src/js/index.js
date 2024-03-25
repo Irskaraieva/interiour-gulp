a33692c052cbbde4dda69c8a01582d8b03087b9f
Irskaraieva <mishytkina14@gmail.com>
2024-03-25 15:42:41 +0100

    fix js drop down list

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,8 @@
-import mobileNav from './modules/mobile-nav.js';
 import range from './modules/range.js';
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 const swiper = new Swiper('.swiper', {
-
 
     spaceBetween: 18,
 
@@ -23,19 +21,9 @@ const swiper = new Swiper('.swiper', {
         },
     },
 
-    scrollbar: {
-        el: ".swiper-scrollbar",
-        draggable: true,
-        snapOnRelease: true,
-    },
-
     navigation: {
         nextEl: '.button-next',
         prevEl: '.button-prev',
-    },
-
-    scrollbar: {
-        el: '.swiper-scrollbar',
     },
 });
 
@@ -64,13 +52,10 @@ const secondSwiper = new Swiper('.second-swiper', {
             slidesPerView: 2.5,
             slidesPerGroup: 1,
         },
-
-
         340: {
             slidesPerView: 1.5,
             slidesPerGroup: 1,
         },
-
         280: {
             slidesPerView: 1,
             slidesPerGroup: 1,
@@ -78,34 +63,10 @@ const secondSwiper = new Swiper('.second-swiper', {
     },
 
     scrollbar: {
-        el: '.swiper-scrollbar',
+        el: ".swiper-scrollbar",
+        draggable: true,
+        snapOnRelease: true,
     },
-});
-
-const nav = document.getElementById('nav');
-const navMobBtn = document.getElementById('nav-mob-btn');
-const navLinks = document.querySelectorAll('.nav-list li');
-
-mobileNav();
-range();
-
-navLinks.forEach(li => {
-    li.addEventListener('click', () => {
-        if (window.innerWidth < 1000) {
-            navMobBtn.classList.toggle('active');
-            document.body.classList.remove('no-scroll');
-        }
-    });
-});
-
-window.addEventListener('resize', function () {
-    if (window.innerWidth > 1000) {
-        document.body.classList.remove('no-scroll');
-        navMobBtn.classList.remove('active');
-        nav.style.display = 'flex'
-    } else if ((!navMobBtn.classList.contains('active')) && (window.innerWidth < 1000)) {
-        nav.style.display = 'none';
-    }
 });
 
 const selectWrapper = document.getElementById('select-wrapper');
@@ -116,10 +77,11 @@ const selectedItem = document.querySelector('.selected-item');
 selectWrapper.addEventListener('click', function (event) {
     const target = event.target;
     const isVisible = select.classList.contains('is-visible');
+    console.log("click");
 
     if (target.tagName === 'LI') {
         selectedItem.textContent = target.textContent;
-    }
+    };
 
     if (isVisible) {
         select.classList.remove('is-visible');
@@ -127,12 +89,5 @@ selectWrapper.addEventListener('click', function (event) {
     } else {
         select.classList.add('is-visible');
         sortArrow.classList.add('rotate');
-    }
+    };
 });
-
-
-
-
-
-
-
