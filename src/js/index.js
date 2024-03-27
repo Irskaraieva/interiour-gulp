5b6fe0ae6c6bbc19c80fd3019b8dc86cb6207d03
Irskaraieva <mishytkina14@gmail.com>
2024-03-27 10:09:40 +0100

    body no-scroll with open filter

--- src/js/index.js
+++ src/js/index.js
@@ -99,6 +99,15 @@ const filterButton = document.getElementById('filter-button');
 filterButton.addEventListener('click', () => {
     filterButton.classList.toggle('active');
     filterWrapper.classList.toggle('active');
+    document.body.classList.toggle('no-scroll');
+});
+
+window.addEventListener('resize', () => { 
+    if (window.innerWidth > 1000) {        
+        filterButton.classList.remove('active');
+        filterWrapper.classList.remove('active');
+        document.body.classList.remove('no-scroll');
+    }
 });
 
 //open store list
